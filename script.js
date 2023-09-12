939061b8ed0367b02c3f7bd2be48bb5a5ec97456
Siddhant <mahatosiddhant69@gmail.com>
2023-09-12 16:54:48 +0530

    change

---
+++ script.js
@@ -0,0 +1,13 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//     var owl = document.querySelector('.owl-carousel');
+//     if (owl) {
+//         var carousel = new Owl.Carousel(owl, {
+//             items: 1,
+//             loop: true,
+//             margin: 10,
+//             autoplay: true,
+//             autoplayTimeout: 3000,
+//             autoplayHoverPause: true
+//         });
+//     }
+// });
